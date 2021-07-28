4118140b9e513f2e03cc3880d4cfed61bb3ff25c
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-07-28 16:17:23 +0300

    deadfish

---
+++ CodeWars/SwimDeadfish/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWars.SwimDeadfish
+{
+	public class Kata
+	{
+		public static int[] Parse(string data)
+		{
+			var value = 0;
+			var result = new List<int>();
+			foreach (var command in data)
+			{
+				switch (command)
+				{
+					case 'i':
+						value++;
+						break;
+					case 'd':
+						value--;
+						break;
+					case 's':
+						value *= value;
+						break;
+					case 'o':
+						result.Add(value);
+						break;
+				}
+			}
+			return result.ToArray();
+		}
+	}
+}
