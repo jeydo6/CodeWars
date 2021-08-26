30d92f920e440ff1ea0b0c68999c5b38f555347d
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-08-26 11:34:19 +0300

    EvenOrOdd

---
+++ CodeWars/EvenOrOdd/Kata.cs
@@ -0,0 +1,10 @@
+namespace CodeWars.EvenOrOdd
+{
+    public class Kata
+    {
+        public static string EvenOrOdd(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
